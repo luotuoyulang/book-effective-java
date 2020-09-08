21d2876a3a094229fe7c5f4aaafe1de05c5ff2e9
luotuoyulang <17600520726@176.com>
2020-09-08 08:15:00 +0800

    ‘’

---
+++ src/main/java/cn/luotuoyulang/effective/BookEffectiveJavaApplication.java
@@ -0,0 +1,13 @@
+package cn.luotuoyulang.effective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookEffectiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/luotuoyulang/effective/BookEffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.luotuoyulang.effective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
